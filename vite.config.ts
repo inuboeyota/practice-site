cfb3c4465da6708dea4dcea2929a388b5d4ced53
inuboeyota <inuboeprog@outlook.com>
2024-08-15 23:41:38 +0900

    [fix] change config base setting

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   // 下記を追加
-  base: process.env.NODE_ENV === 'production' ? '/github-pages-test/' : './',
+  base: process.env.NODE_ENV === 'production' ? '/practice-site/' : './',
   plugins: [vue()],
 })
